d995fc28b3f416eaedcc00fca9c41a7ee434dbb1
Florian <floriandelafosse@gmail.com>
2024-02-07 09:58:47 +0100

    initialisation du projet

---
+++ back-end/app.js
@@ -0,0 +1,20 @@
+const expres = require('express');
+const app = expres();
+const PORT = process.env.PORT || 3000;
+
+//mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/ecf_project_management');
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Erreur de connexion à MongoDB'));
+db.once('open', () => {
+    console.log("Coonecté à Mongo DB");
+});
+
+app.use(expres.json());
+
+//On lance le serveur
+app.listen(PORT, () => {
+    console.log(`serveur en route sur http://localhost:${PORT}`);
+});
